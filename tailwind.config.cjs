c349de15f96e33a24fac0a4f8e8f3d29d7f5db93
Mazdak <nazemi.works@gmail.com>
2022-09-06 23:23:08 +0200

    more on , add stuff

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts}",
+    "./pages/**/*.{html,js}",
+    './components/**/*.{html,js}',
+    './index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
